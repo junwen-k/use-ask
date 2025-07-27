5d84d596ae43e89235396d2970bd8d939127757e
junwen-k <40173716+junwen-k@users.noreply.github.com>
2025-07-27 22:01:45 +0800

    refactor: remove old packages and apps

--- examples/shadcn-ui-nextjs/src/lib/utils.ts
+++
@@ -1,6 +0,0 @@
-import { type CxOptions, cx } from 'class-variance-authority'
-import { twMerge } from 'tailwind-merge'
-
-export function cn(...inputs: CxOptions) {
-  return twMerge(cx(inputs))
-}
--- packages/use-ask/src/index.ts
+++
@@ -1,5 +0,0 @@
-'use client';
-
-export * from './ask-store';
-export * from './create-ask';
-export * from './use-ask';
