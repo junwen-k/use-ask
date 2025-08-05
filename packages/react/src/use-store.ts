67e0527fe2ddcdeb8f667a86b46780feb2bf5b03
junwen-k <40173716+junwen-k@users.noreply.github.com>
2025-08-05 23:03:47 +0800

    feat(react): update react

--- packages/react/src/use-store.ts
+++ packages/react/src/use-store.ts
@@ -1,7 +1,26 @@
-import type { SingletonCallStore } from '@ui-call/core';
+import type { CallStore, SingletonCallStore } from '@ui-call/core';
 import { useSyncExternalStore } from 'react';
 
 const EVENTS = ['add', 'update', 'settled', 'resolve', 'reject'] as const;
+
+export function useCallStore<TPayload = unknown, TData = unknown, TReason = unknown>(
+  store: CallStore<TPayload, TData, TReason>
+) {
+  return useSyncExternalStore(
+    (listener) => {
+      EVENTS.forEach((event) => {
+        store.addEventListener(event, listener);
+      });
+      return () => {
+        EVENTS.forEach((event) => {
+          store.removeEventListener(event, listener);
+        });
+      };
+    },
+    () => store.stack,
+    () => store.stack
+  );
+}
 
 export function useSingletonCallStore<TPayload = unknown, TData = unknown, TReason = unknown>(
   store: SingletonCallStore<TPayload, TData, TReason>
