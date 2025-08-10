f34c1be5eafb74dac7d1d504c09fa1f39b209206
junwen-k <40173716+junwen-k@users.noreply.github.com>
2025-08-10 16:25:43 +0800

    docs(solid): update solid docs

--- apps/solid-demo/src/app.tsx
+++ apps/solid-demo/src/app.tsx
@@ -1,22 +1,10 @@
-import { createSignal } from "solid-js";
-import "./app.css";
+import { Button } from '@/components/ui/button';
+import './app.css';
 
 export default function App() {
-  const [count, setCount] = createSignal(0);
-
   return (
     <main>
-      <h1>Hello world!</h1>
-      <button class="increment" onClick={() => setCount(count() + 1)} type="button">
-        Clicks: {count()}
-      </button>
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
+      <Button>Click me</Button>
     </main>
   );
 }
